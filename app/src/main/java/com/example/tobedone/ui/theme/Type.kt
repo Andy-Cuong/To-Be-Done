e304da20d46b86eab7a30aa4151891e7ae5416f3
Andy Cuong <nguyenduccuong11102003@gmail.com>
2025-01-19 09:26:48 +0200

    Update Version Catalog

--- app/src/main/java/com/example/tobedone/ui/theme/Type.kt
+++ app/src/main/java/com/example/tobedone/ui/theme/Type.kt
@@ -2,14 +2,12 @@ package com.example.tobedone.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
 import com.example.tobedone.R
-
-import androidx.compose.ui.text.googlefonts.GoogleFont
-import androidx.compose.ui.text.googlefonts.Font
 
 val provider = GoogleFont.Provider(
     providerAuthority = "com.google.android.gms.fonts",
